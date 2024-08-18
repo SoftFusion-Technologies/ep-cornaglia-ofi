45e958581f9ce4ba845f8555b3675ceff0855aca
IBRagonese <132090743+IBRagonese@users.noreply.github.com>
2024-08-17 22:14:35 -0300

    Implementación de Pricing & PricingCards

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import Header from '../Components/Header'
 import CardSection from '../Components/CardSection';
+
+import PricingCard from '../Components/PricingCard';
+import Pricing from '../Components/Pricing';
 const Home = () => {
   return (
     <div>
       <Header></Header>
       <CardSection></CardSection>
+      <Pricing></Pricing>
     </div>
   );
 }
