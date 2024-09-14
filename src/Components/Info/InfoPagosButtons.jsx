a738b579c5b2b886dc398ab706785d5e9acae601
Benjamin <goosta19802@gmail.com>
2024-09-14 16:24:51 -0300

    aa2

--- src/Components/Info/InfoPagosButtons.jsx
+++ src/Components/Info/InfoPagosButtons.jsx
@@ -1,25 +1,26 @@
 import { Link } from 'react-router-dom';
 
 const InfoPagosButtons = () => {
-    return <div className="flex flex-wrap justify-center gap-6 mb-12">
+    return (
+      <div className="flex flex-wrap justify-center gap-6 mb-12">
         <a
-            href="http://200.43.130.20/cliente/login" 
-            target="_blank" 
-            rel="noopener noreferrer" 
-            className="bg-red-500 text-white py-6 text-xl rounded-lg w-96 ">
-            <span className='ml-4'>
-                Soy usuario - Acceder a mi cuenta
-            </span>
+          href="http://200.43.130.20/cliente/login"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="bg-red-500 text-white py-6 text-xl rounded-lg w-96 "
+        >
+          <span className="ml-4">Soy usuario - Acceder a mi cuenta</span>
         </a>
         <a
-            href='https://api.whatsapp.com/send/?phone=%2B5493865239239&text=Hola%2C+me+gustar%C3%ADa+gestionar+mi+cuenta para el Portal.&type=phone_number&app_absent=0' target="_blank" 
-            rel="noopener noreferrer" 
-            className="bg-gray-400 text-black py-6 text-xl rounded-lg w-96">
-            <span className='ml-3'>
-                Nuevo usuario - Registrarme ahora
-            </span>
+          href="https://api.whatsapp.com/send/?phone=%2B5493865239239&text=Hola%2C+me+gustar%C3%ADa+gestionar+mi+cuenta+para+el+Portal.%0ANombre+completo%3A+%28completar%29%0ADNI%3A+%28completar%29&type=phone_number&app_absent=0"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="bg-gray-400 text-black py-6 text-xl rounded-lg w-96 flex items-center justify-center"
+        >
+          <span>Nuevo usuario - Registrarme ahora</span>
         </a>
-    </div>
+      </div>
+    );
 }
 
 export default InfoPagosButtons;
