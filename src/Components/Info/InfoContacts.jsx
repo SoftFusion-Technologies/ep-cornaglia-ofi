472602946ca1dbeb39030d10e03967006d10f26c
Benjamin <goosta19802@gmail.com>
2024-12-13 10:25:03 -0300

    Cambios de numero y msj

--- src/Components/Info/InfoContacts.jsx
+++ src/Components/Info/InfoContacts.jsx
@@ -1,168 +1,196 @@
-import Sucursal from "../Sucursal";
+import Sucursal from '../Sucursal';
 import { FaPhoneAlt } from 'react-icons/fa';
-import { FaWhatsapp } from "react-icons/fa"
-import PropTypes from 'prop-types';  
-
+import { FaWhatsapp } from 'react-icons/fa';
+import PropTypes from 'prop-types';
 
 const WhatsAppLink = ({ number }) => (
-    <a
-        href={`https://wa.me/${number.replace(/\D/g, '')}`}
-        target="_blank"
-        rel="noopener noreferrer"
-        className="flex items-center"
-    >
-        <FaWhatsapp className="w-6 h-6 mr-1 text-green-500" />
-    </a>
+  <a
+    href={`https://wa.me/${number.replace(/\D/g, '')}`}
+    target="_blank"
+    rel="noopener noreferrer"
+    className="flex items-center"
+  >
+    <FaWhatsapp className="w-6 h-6 mr-1 text-green-500" />
+  </a>
 );
 
 WhatsAppLink.propTypes = {
-    number: PropTypes.string.isRequired  
+  number: PropTypes.string.isRequired
 };
 
 const InfoContacts = () => {
-    const sucursalesData = [
-        {
-          ciudad: 'Famailla',
-          direccion: 'Belgrano 319',
-          telefono: '3863-568662',
-          horarios: 'Lun a Vie 08:00 a 13:00 / 17:00 a 21:00  Sabados 08:30 a 13:00',
-        },
-        {
-          ciudad: 'Monteros',
-          direccion: 'Rivadavia 184',
-          telefono: '3863-690923',
-          horarios: 'Lun a Vie 08:00 a 13:00 / 17:00 a 21:00  Sabados 08:30 a 13:00',
-        },
-        {
-          ciudad: 'Simoca',
-          direccion: 'Gomez Llueca 639',
-          telefono: '3863-690062',
-          horarios: 'Lun a Vie 08:00 a 13:00 / 17:00 a 21:00  Sabados 08:30 a 13:00',
-        },
-        {
-          ciudad: 'La Cocha',
-          direccion: 'San Martin 441',
-          telefono: '3865-752418',
-          horarios: 'Lun a Vie 08:00 a 13:00 / 17:00 a 21:00  Sabados 08:30 a 13:00',
-        },
-      ];
+  const sucursalesData = [
+    {
+      ciudad: 'Famailla',
+      direccion: 'Belgrano 319',
+      telefono: '3863-568662',
+      horarios: 'Lun a Vie 08:00 a 13:00 / 17:00 a 21:00  Sabados 08:30 a 13:00'
+    },
+    {
+      ciudad: 'Monteros',
+      direccion: 'Rivadavia 184',
+      telefono: '3863-690923',
+      horarios: 'Lun a Vie 08:00 a 13:00 / 17:00 a 21:00  Sabados 08:30 a 13:00'
+    },
+    {
+      ciudad: 'Simoca',
+      direccion: 'Gomez Llueca 639',
+      telefono: '3863-690062',
+      horarios: 'Lun a Vie 08:00 a 13:00 / 17:00 a 21:00  Sabados 08:30 a 13:00'
+    },
+    {
+      ciudad: 'La Cocha',
+      direccion: 'San Martin 441',
+      telefono: '3865-752418',
+      horarios: 'Lun a Vie 08:00 a 13:00 / 17:00 a 21:00  Sabados 08:30 a 13:00'
+    }
+  ];
   return (
-  <div className="container mx-auto p-10 max-w-full overflow-hidden">
-        <h1 className="text-4xl font-extrabold text-gray-900 mb-8 text-center">Nuestras Oficinas</h1>
-         <div className="bg-white p-10 mt-8 rounded-lg border border-gray-200">
-            <h2 className='text-2xl font-bold text-gray-800'>Concepción</h2>
-            <div className="mb-4">
-            <p className="text-gray-600"><strong>Dirección:</strong> Sarmiento 48</p>
-            <p className="text-gray-600">
-                <strong className=" mr-1">Horarios de Atención:</strong>
-                <span className="block md:inline mr-2">Lun a Vie: 08:00 a 13:00 / 17:00 a 21:00</span>
-                <span className="block md:inline">Sábados: 08:30 a 13:00</span>
-            </p>
-            </div>
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
-                <div className="bg-gray-50 p-4 rounded-lg border border-gray-300 hover:shadow-lg transition-shadow duration-300">
-                    <p className="text-gray-600 space-y-2">
-                        <strong>Atención al Cliente:</strong>
-                            <span className="flex items-center">
-                                <a href="tel:3865425787" className="flex items-center">
-                                    <FaPhoneAlt className="w-5 h-5 text-gray-500 mr-2" />3865-425787
-                                </a>
-                            </span>
-                            <span className="flex items-center">
-                                <a href="tel:3865423441" className="flex items-center">
-                                    <FaPhoneAlt className="w-5 h-5 text-gray-500 mr-2" />3865-423441
-                                </a>
-                            </span>
-                    </p>    
-                </div>
-
-                <div className="bg-gray-50 p-4 rounded-lg border border-gray-300 hover:shadow-lg transition-shadow duration-300">
-                    <p className="text-gray-600 space-y-2">
-                        <strong>Servicio Técnico:</strong>
-                        <span className="flex items-center">
-                            <a href={`https://wa.me/5493865219096`} 
-                               className="flex items-center" 
-                               target="_blank" 
-                               rel="noopener noreferrer">
-                                <WhatsAppLink number="+5493865219096" />3865-219096
-                            </a>
-                        </span>
-                        <span className="flex items-center">
-                            <a href={`https://wa.me/5493865320780`} 
-                               className="flex items-center" 
-                               target="_blank" 
-                               rel="noopener noreferrer">
-                                <WhatsAppLink number="+5493865320780" />3865-320780
-                            </a>
-                        </span>
-                    </p>
-                </div>
-
-                <div className="bg-gray-50 p-4 rounded-lg border border-gray-300 hover:shadow-lg transition-shadow duration-300">
-                     <p className="text-gray-600 space-y-2">
-                        <strong>Pagos y Facturas:</strong><br />
-                        <span className="flex items-center">
-                            <a href={`https://wa.me/5493865239239`} 
-                               className="flex items-center" 
-                               target="_blank" 
-                               rel="noopener noreferrer">
-                                <WhatsAppLink number="+5493865239239" />3865-239239
-                            </a>
-                        </span> 
-                    </p>
-                </div>
-
-                <div className="bg-gray-50 p-4 rounded-lg border border-gray-300 hover:shadow-lg transition-shadow duration-300">
-                    <p className="text-gray-600 space-y-2">
-                        <strong>Ventas:</strong><br />
-                        <span className="flex items-center">
-                            <a href={`https://wa.me/5493865266909`} 
-                               className="flex items-center" 
-                               target="_blank" 
-                               rel="noopener noreferrer">
-                                <WhatsAppLink number="+5493865266909" />3865-266909
-                            </a>
-                        </span> 
-                    </p>
-                </div>
-            </div>
-        </div>
-        <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-4 mt-10">
-            {sucursalesData.map((sucursal, index) => (
-            <Sucursal
-                key={index}
-                ciudad={sucursal.ciudad}
-                direccion={sucursal.direccion}
-                telefono={sucursal.telefono}
-                horarios={sucursal.horarios}
-            />
-     ))}
-       </div>
+    <div className="container mx-auto p-10 max-w-full overflow-hidden">
+      <h1 className="text-4xl font-extrabold text-gray-900 mb-8 text-center">
+        Nuestras Oficinas
+      </h1>
       <div className="bg-white p-10 mt-8 rounded-lg border border-gray-200">
-            <h2 className='text-2xl font-bold text-gray-800'>Los Altos</h2>
-            <div className="mb-4">
-            <p className="text-gray-600"><strong>Dirección:</strong> Av. San Martín  270</p>
-            <div className="text-gray-600 mt-2">
-
-                <div className="flex flex-wrap items-center mt-1">
-                <strong className="mr-1">Horarios de Atención:</strong>
-                    <span className="inline-block mr-2"> Lun a Vie 08:00 a 13:00 / 17:00 a 21:00 </span>
-                    <span className="inline-block">Sábados: 08:30 a 13:00</span>
-                </div>
-            </div>
-            <div className="flex items-center mt-2">
-                <strong className="text-gray-600 mr-2">Teléfono:</strong>
-                <span className="flex items-center">
-                    <a href="tel:3834926725" className="flex items-center text-gray-600">
-                        <FaPhoneAlt className="w-5 h-5 text-gray-500 mr-2" /> 383-4926725
-                    </a>
-                </span>
-            </div>
-            </div>
-
+        <h2 className="text-2xl font-bold text-gray-800">Concepción</h2>
+        <div className="mb-4">
+          <p className="text-gray-600">
+            <strong>Dirección:</strong> Sarmiento 48
+          </p>
+          <p className="text-gray-600">
+            <strong className=" mr-1">Horarios de Atención:</strong>
+            <span className="block md:inline mr-2">
+              Lun a Vie: 08:00 a 13:00 / 17:00 a 21:00
+            </span>
+            <span className="block md:inline">Sábados: 08:30 a 13:00</span>
+          </p>
         </div>
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
+          <div className="bg-gray-50 p-4 rounded-lg border border-gray-300 hover:shadow-lg transition-shadow duration-300">
+            <p className="text-gray-600 space-y-2">
+              <strong>Atención al Cliente:</strong>
+              <span className="flex items-center">
+                <a href="tel:3865425787" className="flex items-center">
+                  <FaPhoneAlt className="w-5 h-5 text-gray-500 mr-2" />
+                  3865-425787
+                </a>
+              </span>
+              <span className="flex items-center">
+                <a href="tel:3865423441" className="flex items-center">
+                  <FaPhoneAlt className="w-5 h-5 text-gray-500 mr-2" />
+                  3865-423441
+                </a>
+              </span>
+            </p>
+          </div>
+
+          <div className="bg-gray-50 p-4 rounded-lg border border-gray-300 hover:shadow-lg transition-shadow duration-300">
+            <p className="text-gray-600 space-y-2">
+              <strong>Servicio Técnico:</strong>
+              <span className="flex items-center">
+                <a
+                  href={`https://wa.me/5493865239239`}
+                  className="flex items-center"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <WhatsAppLink number="+5493865239239" />
+                  3865-239239
+                </a>
+              </span>
+              <span className="flex items-center">
+                <a
+                  href={`https://wa.me/5493865239239`}
+                  className="flex items-center"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <WhatsAppLink number="+5493865239239" />
+                  3865-239239{' '}
+                </a>
+              </span>
+            </p>
+          </div>
+
+          <div className="bg-gray-50 p-4 rounded-lg border border-gray-300 hover:shadow-lg transition-shadow duration-300">
+            <p className="text-gray-600 space-y-2">
+              <strong>Pagos y Facturas:</strong>
+              <br />
+              <span className="flex items-center">
+                <a
+                  href={`https://wa.me/5493865239239`}
+                  className="flex items-center"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <WhatsAppLink number="+5493865239239" />
+                  3865-239239
+                </a>
+              </span>
+            </p>
+          </div>
+
+          <div className="bg-gray-50 p-4 rounded-lg border border-gray-300 hover:shadow-lg transition-shadow duration-300">
+            <p className="text-gray-600 space-y-2">
+              <strong>Ventas:</strong>
+              <br />
+              <span className="flex items-center">
+                <a
+                  href={`https://wa.me/5493865239239`}
+                  className="flex items-center"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <WhatsAppLink number="+5493865239239" />
+                  3865-239239
+                </a>
+              </span>
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-4 mt-10">
+        {sucursalesData.map((sucursal, index) => (
+          <Sucursal
+            key={index}
+            ciudad={sucursal.ciudad}
+            direccion={sucursal.direccion}
+            telefono={sucursal.telefono}
+            horarios={sucursal.horarios}
+          />
+        ))}
+      </div>
+      <div className="bg-white p-10 mt-8 rounded-lg border border-gray-200">
+        <h2 className="text-2xl font-bold text-gray-800">Los Altos</h2>
+        <div className="mb-4">
+          <p className="text-gray-600">
+            <strong>Dirección:</strong> Av. San Martín 270
+          </p>
+          <div className="text-gray-600 mt-2">
+            <div className="flex flex-wrap items-center mt-1">
+              <strong className="mr-1">Horarios de Atención:</strong>
+              <span className="inline-block mr-2">
+                {' '}
+                Lun a Vie 08:00 a 13:00 / 17:00 a 21:00{' '}
+              </span>
+              <span className="inline-block">Sábados: 08:30 a 13:00</span>
+            </div>
+          </div>
+          <div className="flex items-center mt-2">
+            <strong className="text-gray-600 mr-2">Teléfono:</strong>
+            <span className="flex items-center">
+              <a
+                href="tel:3834926725"
+                className="flex items-center text-gray-600"
+              >
+                <FaPhoneAlt className="w-5 h-5 text-gray-500 mr-2" />{' '}
+                383-4926725
+              </a>
+            </span>
+          </div>
+        </div>
+      </div>
     </div>
-);
+  );
 };
 
-export default InfoContacts
+export default InfoContacts;
--- src/Pages/GestServicios.jsx
+++ src/Pages/GestServicios.jsx
@@ -1,78 +1,79 @@
 import { Link } from 'react-router-dom';
-import { FaCloudUploadAlt,FaFileInvoiceDollar, FaHeadset } from "react-icons/fa";
+import {
+  FaCloudUploadAlt,
+  FaFileInvoiceDollar,
+  FaHeadset
+} from 'react-icons/fa';
 import { useNavigate } from 'react-router-dom';
 
 const GestServicios = () => {
+  const navigate = useNavigate();
 
-    const navigate = useNavigate();
-
-    const handleClick = () => {
-        navigate('/portal');
-    };
+  const handleClick = () => {
+    navigate('/portal');
+  };
   return (
-    <div className='grid grid-cols-1 gap-6 pt-20 max-w-full' id='gestion'>
-        <div className='bg-gray-50 shadow-md rounded-lg p-6'>
-            <h3 className='text-2xl font-bold mb-4'>
-                Gestiona tus servicios
-            </h3>
-            <div className='mb-6'>
-                <p className='text-gray-600'>
-                    <strong>Con Conectate Wifi</strong>, gestioná tus servicios desde donde quieras.
-                </p>
-            </div>
-            <div className='flex justify-between bg-gray-100 p-6 overflow-hidden'>
-                <ul className='space-y-4 sm:space-y-0 sm:grid sm:grid-cols-3 sm:gap-6 w-full'>
-                  <div className='flex items-center bg-white p-4 rounded-lg shadow-sm flex-grow hover:shadow-lg transition-shadow duration-300 ease-in-out'>
-                    <a
-                        href="https://api.whatsapp.com/send?phone=5493865219096&text=Hola%20quiero%20modificar%20mi%20plan,%20mis%20datos%20son:%20TITULAR:%20(completar)%20DNI:%20(completar)"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        className='flex-shrink-0'
-                    >
-                        <div className='w-12 h-12 bg-gray-100 rounded-full flex items-center justify-center'>
-                        <FaCloudUploadAlt size="2em" />
-                        </div>
-                    </a>
-                    <a
-                        href="https://api.whatsapp.com/send?phone=5493865219096&text=Hola%20quiero%20modificar%20mi%20plan,%20mis%20datos%20son:%20TITULAR:%20(completar)%20DNI:%20(completar)"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        className='ml-4'
-                    >
-                        <div>
-                        <p className='text-lg font-medium'>Mejorar mi plan</p>
-                        </div>
-                    </a>
-                    </div>
-
-
-                    <div className='flex items-center bg-white p-4 rounded-lg shadow-sm flex-grow hover:shadow-lg transition-shadow duration-300 ease-in-out'>
-                        <Link to="/portal" className='flex-shrink-0'>
-                            <div className='w-12 h-12 bg-gray-100 rounded-full flex items-center justify-center'>
-                                {<FaFileInvoiceDollar size="2em"/>}
-                            </div>
-                          </Link>
-                        <div onClick={handleClick} className='ml-4 cursor-pointer'>
-                            <p className='text-lg font-medium'>Mis facturas</p>
-                        </div>
-                    </div>
-                    <div className='flex items-center bg-white p-4 rounded-lg shadow-sm flex-grow hover:shadow-lg transition-shadow duration-300 ease-in-out'>
-                        <Link to="/autogestion" className='flex-shrink-0'>
-                            <div className='w-12 h-12 bg-gray-100 rounded-full flex items-center justify-center'>
-                                {<FaHeadset size="2em"/>}
-                            </div>
-                          </Link>
-                          <Link to="/autogestion">
-                        <div className='ml-4'>
-                            <p className='text-lg font-medium'>Asistencia Técnica</p>
-                              </div>
-                          </Link>
-                    </div>
-                </ul>
-            </div>
+    <div className="grid grid-cols-1 gap-6 pt-20 max-w-full" id="gestion">
+      <div className="bg-gray-50 shadow-md rounded-lg p-6">
+        <h3 className="text-2xl font-bold mb-4">Gestiona tus servicios</h3>
+        <div className="mb-6">
+          <p className="text-gray-600">
+            <strong>Con Conectate Wifi</strong>, gestioná tus servicios desde
+            donde quieras.
+          </p>
         </div>
-    </div>
-  )
-}
+        <div className="flex justify-between bg-gray-100 p-6 overflow-hidden">
+          <ul className="space-y-4 sm:space-y-0 sm:grid sm:grid-cols-3 sm:gap-6 w-full">
+            <div className="flex items-center bg-white p-4 rounded-lg shadow-sm flex-grow hover:shadow-lg transition-shadow duration-300 ease-in-out">
+              <a
+                href="https://api.whatsapp.com/send?phone=5493865239239&text=Hola"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="flex-shrink-0"
+              >
+                <div className="w-12 h-12 bg-gray-100 rounded-full flex items-center justify-center">
+                  <FaCloudUploadAlt size="2em" />
+                </div>
+              </a>
+              <a
+                href="https://api.whatsapp.com/send?phone=5493865219096&text=Hola"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="ml-4"
+              >
+                <div>
+                  <p className="text-lg font-medium">Mejorar mi plan</p>
+                </div>
+              </a>
+            </div>
 
-export default GestServicios
+            <div className="flex items-center bg-white p-4 rounded-lg shadow-sm flex-grow hover:shadow-lg transition-shadow duration-300 ease-in-out">
+              <Link to="/portal" className="flex-shrink-0">
+                <div className="w-12 h-12 bg-gray-100 rounded-full flex items-center justify-center">
+                  {<FaFileInvoiceDollar size="2em" />}
+                </div>
+              </Link>
+              <div onClick={handleClick} className="ml-4 cursor-pointer">
+                <p className="text-lg font-medium">Mis facturas</p>
+              </div>
+            </div>
+            <div className="flex items-center bg-white p-4 rounded-lg shadow-sm flex-grow hover:shadow-lg transition-shadow duration-300 ease-in-out">
+              <Link to="/autogestion" className="flex-shrink-0">
+                <div className="w-12 h-12 bg-gray-100 rounded-full flex items-center justify-center">
+                  {<FaHeadset size="2em" />}
+                </div>
+              </Link>
+              <Link to="/autogestion">
+                <div className="ml-4">
+                  <p className="text-lg font-medium">Asistencia Técnica</p>
+                </div>
+              </Link>
+            </div>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default GestServicios;
