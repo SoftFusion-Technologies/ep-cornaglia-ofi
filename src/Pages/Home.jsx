5a52b01a5d6f174db48c8a5012dcc122461e4531
Benjamin <goosta19802@gmail.com>
2025-01-31 10:17:36 -0300

    Se corrije el home

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,4 +1,4 @@
-import React,{useState,useEffect} from 'react';
+import { useEffect, useRef } from 'react';
 import Header from '../Components/Header/Header';
 import CardSection from '../Components/Cards/CardSection';
 import { useNavigate } from 'react-router-dom';
@@ -14,16 +14,31 @@ import HBOImages from '../Components/HBOImages';
 import WhatsAppContact from '../Components/Info/WhatsAppContact';
 
 const Home = () => {
-  const navigate = useNavigate(); // Obtén el hook de navegación
+  const navigate = useNavigate();
 
   useEffect(() => {
-    const timer = setTimeout(() => {
-      navigate('/promociones'); // Redirige a /promociones
-    }, 2000);
+    // Verifica si ya se ha redirigido en esta sesión
+    if (!localStorage.getItem('hasRedirected')) {
+      const timer = setTimeout(() => {
+        navigate('/promociones'); // Redirige a /promociones
+        localStorage.setItem('hasRedirected', 'true'); // Marca que ya se ha redirigido
+      }, 2000);
 
-    return () => clearTimeout(timer); // Limpieza del temporizador
+      return () => clearTimeout(timer); // Limpieza del temporizador
+    }
+
+    // Limpiar el localStorage cuando la página se cierre o se recargue
+    const handleBeforeUnload = () => {
+      localStorage.removeItem('hasRedirected');
+    };
+
+    window.addEventListener('beforeunload', handleBeforeUnload);
+
+    // Cleanup: Remover el event listener cuando el componente se desmonte
+    return () => {
+      window.removeEventListener('beforeunload', handleBeforeUnload);
+    };
   }, [navigate]);
-
 
   return (
     <div>
