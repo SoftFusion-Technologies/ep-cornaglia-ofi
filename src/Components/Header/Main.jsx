7977a38e73bbf2a581310856a34fbeb40fa1122b
Benjamin <goosta19802@gmail.com>
2025-04-26 14:46:32 -0300

    mejoraaaaaaas

--- src/Components/Header/Main.jsx
+++ src/Components/Header/Main.jsx
@@ -7,8 +7,8 @@ const Main = () => {
       id="quienesSomos"
       className="flex flex-col md:flex-row items-center justify-center bg-white p-8 md:p-16"
     >
-      {/* Se agrega animación de entrada */}
-      <div className="md:w-1/2 w-full text-center md:text-left animate__animated animate__fadeIn animate__delay-300ms">
+      {/* Contenedor de texto con fondo blanco, borde redondeado y altura mínima */}
+      <div className="md:w-1/2 w-full text-center md:text-left animate__animated animate__fadeIn animate__delay-300ms rounded-lg bg-white p-6 shadow-lg min-h-[24rem] transition-transform duration-300 ease-in-out hover:scale-105 hover:shadow-2xl">
         <h2 className="text-4xl font-bold text-gray-800 mb-4 animate__animated animate__fadeIn animate__delay-500ms">
           ¿QUIÉNES SOMOS?
         </h2>
@@ -18,10 +18,18 @@ const Main = () => {
           nuestro municipio y alrededores. Contamos con un equipo capacitado que
           instala servicios modernos y utiliza la última tecnología para
           garantizarte la mejor experiencia en internet.
+          <span className="md:block">
+            <strong className="text-red-600"> Hemos logrado </strong>extender
+            nuestra cobertura a áreas rurales y de difícil acceso, como los
+            cerros tucumanos, incluyendo zonas como el Indio. Tambien por
+            Atahona y Monteagudo en la ciudad de Simoca. Esta expansión nos ha
+            permitido llevar WiFi de alta calidad a lugares donde antes no
+            llegaba, mejorando la conectividad de muchas familias y empresas.
+          </span>
         </p>
       </div>
 
-      {/* Se agrega animación de entrada con transformaciones */}
+      {/* Contenedor de imagen */}
       <div className="md:w-1/2 w-full flex justify-center md:justify-end mb-8 md:mb-0 md:mr-28">
         <img
           src={FotoPerson}
--- src/Pages/GestServicios.jsx
+++ src/Pages/GestServicios.jsx
@@ -12,6 +12,7 @@ const GestServicios = () => {
   const handleClick = () => {
     navigate('/portal');
   };
+
   return (
     <div id="gestion">
       <div className="grid grid-cols-1 gap-6 pt-20 max-w-full">
@@ -23,14 +24,19 @@ const GestServicios = () => {
               donde quieras.
             </p>
           </div>
+
           <div className="flex justify-between bg-gray-100 p-6 overflow-hidden">
             <ul className="space-y-4 sm:space-y-0 sm:grid sm:grid-cols-3 sm:gap-6 w-full">
-              <div className="flex items-center bg-white p-4 rounded-lg shadow-sm flex-grow hover:shadow-lg transition-shadow duration-300 ease-in-out">
+              {/* Servicio Administrativo */}
+              <div className="relative group overflow-hidden flex items-center bg-white p-4 rounded-lg shadow-sm flex-grow hover:shadow-lg transition-shadow duration-300 ease-in-out cursor-pointer">
+                {/* Mancha */}
+                <div className="absolute w-20 h-20 bg-red-500 rounded-full bottom-0 right-0 transform translate-x-full translate-y-full scale-0 group-hover:translate-x-0 group-hover:translate-y-0 group-hover:scale-[20] transition-all duration-[1000ms] ease-out"></div>
+
                 <a
                   href="https://api.whatsapp.com/send?phone=5493865239239&text=Hola"
                   target="_blank"
                   rel="noopener noreferrer"
-                  className="flex-shrink-0"
+                  className="flex-shrink-0 z-10"
                 >
                   <div className="w-12 h-12 bg-gray-100 rounded-full flex items-center justify-center">
                     <FaCloudUploadAlt size="2em" />
@@ -40,20 +46,24 @@ const GestServicios = () => {
                   href="https://api.whatsapp.com/send?phone=5493865239239&text=Hola"
                   target="_blank"
                   rel="noopener noreferrer"
-                  className="ml-4"
+                  className="ml-4 z-10"
                 >
                   <div>
-                    <p className="text-lg font-medium">
+                    <p className="text-lg font-medium text-black transition-colors duration-700 group-hover:text-white">
                       Servicio Administrativo
                     </p>
                   </div>
                 </a>
               </div>
 
-              <div className="flex items-center bg-white p-4 rounded-lg shadow-sm flex-grow hover:shadow-lg transition-shadow duration-300 ease-in-out">
-                <Link to="/autogestion" className="flex-shrink-0">
+              {/* Servicio Técnico */}
+              <div className="relative group overflow-hidden flex items-center bg-white p-4 rounded-lg shadow-sm flex-grow hover:shadow-lg transition-shadow duration-300 ease-in-out cursor-pointer">
+                {/* Mancha */}
+                <div className="absolute w-20 h-20 bg-red-500 rounded-full bottom-0 right-0 transform translate-x-full translate-y-full scale-0 group-hover:translate-x-0 group-hover:translate-y-0 group-hover:scale-[20] transition-all duration-[1000ms] ease-out"></div>
+
+                <Link to="/autogestion" className="flex-shrink-0 z-10">
                   <div className="w-12 h-12 bg-gray-100 rounded-full flex items-center justify-center">
-                    {<FaHeadset size="2em" />}
+                    <FaHeadset size="2em" />
                   </div>
                 </Link>
                 <Link
@@ -61,20 +71,30 @@ const GestServicios = () => {
                   onClick={() =>
                     window.open('https://wa.me/3865239239?text=Hola', '_blank')
                   }
+                  className="ml-4 z-10"
                 >
-                  <div className="ml-4">
-                    <p className="text-lg font-medium">Servicio técnico</p>
+                  <div>
+                    <p className="text-lg font-medium text-black transition-colors duration-700 group-hover:text-white">
+                      Servicio técnico
+                    </p>
                   </div>
                 </Link>
               </div>
-              <div className="flex items-center bg-white p-4 rounded-lg shadow-sm flex-grow hover:shadow-lg transition-shadow duration-300 ease-in-out">
-                <Link to="/portal" className="flex-shrink-0">
+
+              {/* Pagos Electrónicos */}
+              <div className="relative group overflow-hidden flex items-center bg-white p-4 rounded-lg shadow-sm flex-grow hover:shadow-lg transition-shadow duration-300 ease-in-out cursor-pointer">
+                {/* Mancha */}
+                <div className="absolute w-20 h-20 bg-red-500 rounded-full bottom-0 right-0 transform translate-x-full translate-y-full scale-0 group-hover:translate-x-0 group-hover:translate-y-0 group-hover:scale-[20] transition-all duration-[1000ms] ease-out"></div>
+
+                <Link to="/portal" className="flex-shrink-0 z-10">
                   <div className="w-12 h-12 bg-gray-100 rounded-full flex items-center justify-center">
-                    {<FaFileInvoiceDollar size="2em" />}
+                    <FaFileInvoiceDollar size="2em" />
                   </div>
                 </Link>
-                <div onClick={handleClick} className="ml-4 cursor-pointer">
-                  <p className="text-lg font-medium">Pagos Electrónicos</p>
+                <div onClick={handleClick} className="ml-4 z-10">
+                  <p className="text-lg font-medium text-black transition-colors duration-700 group-hover:text-white">
+                    Pagos Electrónicos
+                  </p>
                 </div>
               </div>
             </ul>
