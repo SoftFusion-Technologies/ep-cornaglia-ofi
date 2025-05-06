d32b3f6c7d1c5d1255d9f50075fb71423cf4f96b
Benjamin <goosta19802@gmail.com>
2025-05-06 17:53:29 -0300

    2

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,8 +1,5 @@
-import { useEffect, useRef } from 'react';
 import Header from '../Components/Header/Header';
 import CardSection from '../Components/Cards/CardSection';
-import { useNavigate } from 'react-router-dom';
-
 // import PricingCard from '../Components/PricingCard';
 import Pricing from '../Components/Header/Pricing';
 import Main from '../Components/Header/Main';
@@ -13,40 +10,14 @@ import InfoContacts from '../Components/Info/InfoContacts';
 import HBOImages from '../Components/HBOImages';
 import WhatsAppContact from '../Components/Info/WhatsAppContact';
 
-import Promociones from '../Pages/Promociones';
 import New2 from '../Components/Header/New2';
 import LocalidadesConMapa from '../Components/LocalidadesConMapa';
 const Home = () => {
-  // const navigate = useNavigate();
-
-  // useEffect(() => {
-  //   // Verifica si ya se ha redirigido en esta sesión
-  //   if (!localStorage.getItem('hasRedirected')) {
-  //     const timer = setTimeout(() => {
-  //       navigate('/promociones'); // Redirige a /promociones
-  //       localStorage.setItem('hasRedirected', 'true'); // Marca que ya se ha redirigido
-  //     }, 2000);
-
-  //     return () => clearTimeout(timer); // Limpieza del temporizador
-  //   }
-
-  //   // Limpiar el localStorage cuando la página se cierre o se recargue
-  //   const handleBeforeUnload = () => {
-  //     localStorage.removeItem('hasRedirected');
-  //   };
-
-  //   window.addEventListener('beforeunload', handleBeforeUnload);
-
-  //   // Cleanup: Remover el event listener cuando el componente se desmonte
-  //   return () => {
-  //     window.removeEventListener('beforeunload', handleBeforeUnload);
-  //   };
-  // }, [navigate]);
 
   return (
     <div>
       <Header></Header>
-      {/* <CardSection></CardSection> */}
+       {/* <CardSection></CardSection> */}
       <Main></Main>
       <New2></New2>
       {/* <Promociones></Promociones> */}
